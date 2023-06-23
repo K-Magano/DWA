d4578fa2f4fd30631d3e5d0fbfbcea1e954e02bd
K-Magano <124664588+K-Magano@users.noreply.github.com>
2023-06-23 11:47:13 +0200

    Completed DWA11

---
+++ DWA11/Tally.md
@@ -0,0 +1,15 @@
+# Tally App📱
+ 
+ Here is a simple interactive work-along app created to see the basic uses of JavaScript. 
+
+ ## Features
+
+  - 💚User Friendly 
+  - 🐜Minimal functionality
+  - 💪Ability to count up or down. 
+
+## Requirements
+
+    - IDE [VS Code](https://code.visualstudio.com/).
+    - Basic HTML, CSS and JavaScript knowledge.
+    - A browser like Chrome.
---
+++ DWA11/script.js
@@ -0,0 +1,70 @@
+const initialState = {
+  count: 0
+};
+
+const store = {
+  state: initialState,
+  getState() {
+    return this.state;
+  },
+  setState(newState) {
+    this.state = newState;
+  }
+};
+
+
+const addHandler = () => {
+  const STEP_AMOUNT = 1;
+  const MAX_NUMBER = 10;
+  const number = document.querySelector('[data-key="number"]');
+  const newValue = parseInt(number.value) + STEP_AMOUNT;
+  number.value = newValue;
+
+  const subtract = document.querySelector('[data-key="subtract"]');
+  if (subtract.disabled === true) {
+    subtract.disabled = false;
+  }
+
+  const add = document.querySelector('[data-key="add"]');
+  if (newValue >= MAX_NUMBER) {
+    add.disabled = true;
+  }
+
+  store.setState({ count: newValue });
+};
+
+const subtractHandler = () => {
+  const STEP_AMOUNT = 1;
+  const MIN_NUMBER = 0;
+  const number = document.querySelector('[data-key="number"]')
+  const newValue = parseInt(number.value) - STEP_AMOUNT;
+  number.value = newValue;
+
+  const add = document.querySelector('[data-key="add"]');
+  if (add.disabled === true) {
+    add.disabled = false;
+  }
+
+  const subtract = document.querySelector('[data-key="subtract"]');
+  if (newValue <= MIN_NUMBER) {
+    subtract.disabled = true;
+  }
+
+  store.setState({ count: newValue });
+};
+
+const resetHandler = () => {
+
+  const number = document.querySelector('[data-key="number"]');
+  number.value = 0;
+  store.setState({ count: 0 });
+};
+
+const add = document.querySelector('[data-key="add"]');
+add.addEventListener('click', addHandler);
+
+const subtract = document.querySelector('[data-key="subtract"]');
+subtract.addEventListener('click', subtractHandler);
+
+const reset = document.querySelector('[data-key="reset"]');
+reset.addEventListener('click', resetHandler);
